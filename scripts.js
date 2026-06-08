c7849e7867407e8c71ddc2b0b602e612a84c8973
Vagner Aparecido Barbosa Junior <123818964+VagBrownsp@users.noreply.github.com>
2026-06-08 13:55:53 -0300

    Update scripts.js
    ewwwwwwwwwwww

--- scripts.js
+++ scripts.js
@@ -162,7 +162,7 @@ Valor: ${valorEntrada.value || "Não possui"}
 `;
 
     window.open(
-      "https://wa.me/5512988089776text=" + encodeURIComponent(mensagem),
+      "https://wa.me/5512988089776?text=" + encodeURIComponent(mensagem),
       "_blank"
     );
   });
